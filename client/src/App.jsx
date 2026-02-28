d02b03366fffde675a4a4fb5f04b32a3a1fe3ef3
Shivani Barman <shivanibarman247@gmail.com>
2026-02-28 22:51:16 +0530

    feat: implement routing and add page components for Home, Login, Chat, and Group

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { lazy } from 'react'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Group = lazy(() => import("./pages/Group"));
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="/group" element={<Group />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
