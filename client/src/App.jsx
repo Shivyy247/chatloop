e5a1c705ec34f89edfbaf710911a356f41a5a78f
Shivani Barman <shivanibarman247@gmail.com>
2026-03-03 23:30:28 +0530

    feat: implement ProtectRoute for authentication and add NotFound page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import Group from "./pages/Group";
+import NotFound from "./pages/NotFound";
+
+let user = true;
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/group" element={<Group />} />
-        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route element={<ProtectRoute user={user} />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/group" element={<Group />} />
+          <Route path="/chat/:chatId" element={<Chat />} />
+        </Route>
+
+        <Route
+          path="/login"
+          element={
+            <ProtectRoute user={!user} redirect="/">
+              <Login />
+            </ProtectRoute>
+          }
+        />
+
+        <Route path="*" element={<NotFound />}></Route>
+
+
       </Routes>
     </BrowserRouter>
   );
