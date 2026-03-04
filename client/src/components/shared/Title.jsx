a4b991c57999194dec72111b9fd15562ba34c978
Shivani Barman <shivanibarman247@gmail.com>
2026-03-04 23:30:12 +0530

    feat: add AppLayout component and integrate it into Chat and Home pages; create Title component

---
+++ client/src/components/shared/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Helmet} from "react-helmet-async"
+
+const Title = ({title = "Chat", description = "this is the chat app called Chatloop"}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Title
