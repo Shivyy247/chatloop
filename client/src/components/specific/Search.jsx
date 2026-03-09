625668093b89d07e3dddc95d9cd994928ab223cc
Shivani Barman <shivanibarman247@gmail.com>
2026-03-09 22:37:00 +0530

    search button style completed

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,10 +1,30 @@
-import { Dialog } from '@mui/material'
 import React from 'react'
-
+import { useInputValidation } from "6pp"
+import {Dialog, DialogTitle, InputAdornment, Stack, TextField} from "@mui/material"
+import { Search as SearchIcon } from '@mui/icons-material'
 const Search = () => {
-  return <Dialog open >
-
-  </Dialog>
+  const search = useInputValidation("")
+  return (
+    <Dialog open>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant='outlined'
+          size='small'
+          InputProps={{
+            smartAdornment: (
+              <InputAdornment position='start'>
+                <SearchIcon/>
+              </InputAdornment>
+            )
+          }}
+        />
+      </Stack>
+    </Dialog>
+  )
 }
 
 export default Search
