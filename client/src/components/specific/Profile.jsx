72ba66587b233cc8c09e642a5b1ca342032d8a54
Shivani Barman <shivanibarman247@gmail.com>
2026-03-09 21:55:16 +0530

    feat: enhance Profile component with additional ProfileCard details and icons

--- client/src/components/specific/Profile.jsx
+++ client/src/components/specific/Profile.jsx
@@ -1,23 +1,46 @@
-import { Avatar, Stack } from '@mui/material';
+import { Avatar, Stack, Typography } from '@mui/material';
+import {
+    Face as FaceIcon,
+    AlternateEmail as UserNameIcon,
+    CalendarMonth as CalenderIcon
+} from "@mui/icons-material"
 import React from 'react'
+import moment from "moment"
 
 const Profile = () => {
   return (
-      <Stack spacing={"2rem"} direction={"column"} alignItems={"center"}>
-          <Avatar
-              sx={{
-                  width: 200,
-                  height: 200,
-                  objectFit: "contain",
-                  marginBottom: "1rem",
-                  border: "5px solid white"
-              }}
-          />
-      <ProfileCard heading={"Bio"} text={"kab banega bhai tuu jaldi banega"} />
+    <Stack spacing={"2rem"} direction={"column"} alignItems={"center"}>
+      <Avatar
+        sx={{
+          width: 200,
+          height: 200,
+          objectFit: "contain",
+          marginBottom: "1rem",
+          border: "5px solid white",
+        }}
+      />
+      <ProfileCard heading={"Bio"} text={"kab banega bhai tuu jaldi ban jaa"} />
+      <ProfileCard
+        heading={"Username"}
+        text={"shivyy"}
+        Icon={<UserNameIcon />}
+      />
+      <ProfileCard heading={"Name"} text={"shivi barman"} Icon={<FaceIcon />} />
+      <ProfileCard
+        heading={"Joined"}
+        text={moment('2025-03-09T00:00:00.000Z').fromNow()}
+        Icon={<CalenderIcon />}
+      />
     </Stack>
-  )
+  );
 }
 
-const ProfileCard = ({text, icon, heading}) => <div>ProfileCard</div>;
+const ProfileCard = ({ text, Icon, heading }) => <Stack direction={"row"} alignItems={"center"} spacing={"1rem"} color={"white"} textAlign={"center"}>
+    {Icon && Icon}
+    <Stack>
+        <Typography variant='body1'>{text}</Typography>
+        <Typography color={"gray"} variant='caption' >{heading}</Typography>
+    </Stack>
+</Stack>;
 
 export default Profile
