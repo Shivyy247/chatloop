21cd185128b1332127df8a9665eb7f670bd0ae80
Shivani Barman <shivanibarman247@gmail.com>
2026-03-09 23:17:25 +0530

    feat: implement Search component with user list and friend request functionality; add UserItem component for user display

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useInputValidation } from "6pp"
-import {Dialog, DialogTitle, InputAdornment, Stack, TextField} from "@mui/material"
+import {Dialog, DialogTitle, InputAdornment, List, ListItem, ListItemText, Stack, TextField} from "@mui/material"
 import { Search as SearchIcon } from '@mui/icons-material'
+import UserItem from '../shared/UserItem'
+import { sampleUsers } from '../../constants/sampleData'
+
+
 const Search = () => {
   const search = useInputValidation("")
+
+  let isLoadingSendFriendRequest = false;
+  const [users, setUsers] = useState(sampleUsers);
+
+  const addFriendHandler = (id) => {
+    console.log(id)
+  }
+
+
   return (
     <Dialog open>
       <Stack p={"2rem"} direction={"column"} width={"25rem"}>
@@ -15,13 +28,27 @@ const Search = () => {
           variant='outlined'
           size='small'
           InputProps={{
-            smartAdornment: (
+            smartadornment: (
               <InputAdornment position='start'>
                 <SearchIcon/>
               </InputAdornment>
             )
           }}
         />
+
+        <List>
+          {
+            users.map((i) => (
+              <UserItem
+                user={i}
+                key={i._id}
+                handler={addFriendHandler}
+                handlerIsLoading={isLoadingSendFriendRequest}
+              />
+            ))
+          }
+        </List>
+
       </Stack>
     </Dialog>
   )
--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -28,3 +28,18 @@ export const sampleChats = [
     members: ["1", "2"],
   },
 ];
+
+export const sampleUsers = [
+  {
+    avatar:
+      "https://as1.ftcdn.net/jpg/14/04/65/88/1000_F_1404658802_qSXASYeuQA3yeEzO1ZZzTdP4IE6yGHm3.jpg",
+    name: "nami",
+    _id: 1,
+  },
+  {
+    avatar:
+      "https://static.vecteezy.com/system/resources/previews/037/047/063/non_2x/ai-generated-user-web-icon-symbol-3d-isolated-on-transparent-background-png.png",
+    name: "niya",
+    _id: 2,
+  },
+];
