c077c8dd445c23ee72542676e450b183a0dd8da5
Shivani Barman <shivanibarman247@gmail.com>
2026-03-17 08:28:13 +0530

    feat: add navigation buttons to Group component; implement back navigation and mobile menu toggle

--- client/src/pages/Group.jsx
+++ client/src/pages/Group.jsx
@@ -1,7 +1,56 @@
-import { Grid } from '@mui/material'
+import { Box, Grid, IconButton, MenuItem, Tooltip } from '@mui/material'
+import { KeyboardBackspace as KeyboardBackspaceIcon , Menu as MenuIcon} from "@mui/icons-material"
 import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 const Group = () => {
+
+  const navigate = useNavigate();
+
+  const navigateBack = () => {
+    navigate("/");
+  }
+
+  const handleMobile = () => { };
+
+  const IconBtns = (
+    <>
+      <Box
+        sx={{
+          display: {
+            xs: "block",
+            sm: "none",
+            position: "fixed",
+            right: "1rem",
+            top: "1rem",
+          },
+        }}
+      >
+        <IconButton onClick={handleMobile} >
+          <MenuIcon />
+        </IconButton>
+      </Box>
+
+      <Tooltip title="back">
+        <IconButton
+          sx={{
+            position: "absolute",
+            top: "2rem",
+            left: "2rem",
+            bgcolor: "#1c1c1c",
+            color: "white",
+            ":hover": {
+              bgcolor: "rgba(0,0,0,0.7)",
+            },
+          }}
+          onClick={navigateBack}
+        >
+          <KeyboardBackspaceIcon />
+        </IconButton>
+      </Tooltip>
+    </>
+  );
+
   return (
     <Grid container height={"100vh"} >
     <Grid item sx={{
@@ -22,7 +71,7 @@ const Group = () => {
       position: "relative",
       padding: "1rem 3rem",
     }} >
-      Group Details
+      {IconBtns}
     </Grid>
     </Grid>
   )
