8d0ac70416a8796c621817bad95f418a9fe64d11
Shivani Barman <shivanibarman247@gmail.com>
2026-03-29 14:13:09 +0530

    feat: implement LineChart component with chart options and data structure

--- client/src/components/specific/Charts.jsx
+++ client/src/components/specific/Charts.jsx
@@ -1,11 +1,80 @@
 import React from "react";
 import { Line, Doughnut } from "react-chartjs-2";
+import {
+    CategoryScale,
+    Chart as ChartJS,
+    Tooltip,
+    Filler,
+    LinearScale,
+    PointElement,
+    LineElement,
+    ArcElement,
+    Legend,
+    plugins,
+    scales,
+} from "chart.js";
+
+
+ChartJS.register(
+  CategoryScale,
+  Tooltip,
+  Filler,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Legend,
+);
+
+const lineChartOptions = {
+    responsive: true,
+    plugins: {
+        legend: {
+            display: false,
+        },
+        title: {
+            display: false,
+        },
+    },
+    scales: {
+        x: {
+            grid: {
+                display: false,
+            }
+        },
+        y: {
+            beginAtZero: true,
+            grid: {
+                display: false,
+            },
+        },
+    }
+};
+
 
 const LineChart = () => {
+
+    const data = {
+      labels: ["January", "Febuary", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [1, 2, 34,6],
+          label: "Revenue",
+          fill: false,
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)",
+        },
+        {
+          data: [1, 22, 5, 6],
+          label: "Revenue 2",
+          fill: false,
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)",
+        },
+      ],
+    };
     return (
-        <div>
-            charts
-        </div>
+        <Line data={data} options={lineChartOptions} />
     )
 }
 
