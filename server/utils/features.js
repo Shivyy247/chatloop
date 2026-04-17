5c1b860492dc7332ef01d22ebbfed302e0a0073c
Shivani Barman <shivanibarman247@gmail.com>
2026-04-17 23:47:13 +0530

    added tokens!!

--- server/utils/features.js
+++ server/utils/features.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose"
+import jwt from 'jsonwebtoken'
 
+
+const cookieOptions = {
+        maxAge: 15 * 24 * 60 * 60 * 1000,
+        sameSite: "none",
+        httpOnly: true,
+        secure: true,
+    }
 
 const connectDB = (uri) => {
     mongoose
@@ -13,19 +21,12 @@ const connectDB = (uri) => {
 };
 
 const sendToken = (res, user, code, message) => { 
-    const token = "123456";
+    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
 
-    return res.status(code).cookie("chattu-token", token, {
-        maxAge: 15 * 24 * 60 * 60 * 1000,
-        sameSite: "none",
-        httpOnly: true,
-        secure: true,
-    })
+    return res.status(code).cookie("chattu-token", token, cookieOptions)
         .json({
         success: true,
-        token,
         message,
-        user
     });
 };
 
