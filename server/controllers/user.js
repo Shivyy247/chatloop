6e2a88f85851ba14f74124dbe2eaad047a8718eb
Shivani Barman <shivanibarman247@gmail.com>
2026-04-26 23:06:17 +0530

    feat: add search user functionality and update user routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -63,5 +63,19 @@ const logout = TryCatch(async (req, res) => {
     });
 });
 
+const searchUser = TryCatch(async (req, res) => {
 
-export { login, newUser, getMyProfile, logout };
+  const { name } = req.query;
+
+
+
+  return res
+    .status(200)
+    .json({
+      success: true,
+      message: name,
+    });
+});
+
+
+export { login, newUser, getMyProfile, logout , searchUser};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getMyProfile, login, newUser } from "../controllers/user.js";
+import { getMyProfile, login, newUser, searchUser } from "../controllers/user.js";
 import { singleAvatar } from "../middlewares/multer.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
@@ -17,6 +17,8 @@ app.use(isAuthenticated);
 app.get("/me", getMyProfile);
 
 app.get("/logout", logout);
+
+app.get("/search", searchUser);
 
 
 export default app
